7645a8bf7be24f2e0e214bc811291694f2999fd9
Nauryzbayev Darmanbek <n.darman05@gmail.com>
2019-12-18 16:24:00 +0600

    Lesson 35. Display all posts - part 1

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class BlogController extends Controller
+{
+    public function index(){
+        $posts = Post::all();
+        return view('blog.index', compact('posts'));
+    }
+}
