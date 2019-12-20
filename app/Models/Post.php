dd7bb95ac32e46dec84f21bcb6201bb9871d81f1
Nauryzbayev Darmanbek <n.darman05@gmail.com>
2019-12-21 00:33:16 +0600

    Lesson41. Enable Markdown Support

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use GrahamCampbell\Markdown\Facades\Markdown;
 
 class Post extends Model
 {
@@ -27,7 +28,17 @@ class Post extends Model
         return $this->created_at->diffForHumans();
     }
 
+    public function getBodyHtmlAttribute($value){
+        return $this->body ? Markdown::convertToHtml(e($this->body)) : NULL ;
+    }
+
+    public function getExcerptHtmlAttribute($value){
+        return $this->excerpt ? Markdown::convertToHtml(e($this->excerpt)) : NULL ;
+    }
+
     public function scopeLatestFirst(){
         return $this->orderBy('created_at', 'desc');
     }
+
+    
 }
